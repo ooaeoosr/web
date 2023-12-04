0f7d06f5c3fd8fedded347f00b1f32a616fe4df7
unknown <you@example.com>
2023-12-04 18:00:31 +0900

    20231204

--- animal.js
+++ animal.js
@@ -1,16 +1,40 @@
 // 즉시실행함수(IIFE)
 // 선언하면서 실행
+// (function() {
+//     function Animal(species, livingPlace){
+//         this.typeOfSpecies = species;
+//         this.whereLives = livingPlace;
+//         this.move = function() {
+//             console.log("animal moves");
+//         }
+//         this.describe = function() {
+//             console.log("species "+this.typeOfSpecies);
+//             console.log("whereLives "+this.whereLives);
+//         };
+//     }
+//         var dog = new Animal("개과", "육지");
+//         var cat = new Animal("고양이과","육지");
+//         dog.describe();
+//         cat.describe();
+    
+// })();
+
 (function() {
+    Animal.prototype.describe = function(){
+        return `${this.typeOfSpecies}  ${this.whereLives}`
+    };
+    Animal.prototype.move = function() {
+        console.log("animal moves");
+    };
     function Animal(species, livingPlace){
         this.typeOfSpecies = species;
         this.whereLives = livingPlace;
-        this.move = function() {
-            console.log("animal moves");
-        }
-        this.describe = function() {
-            console.log("species "+this.typeOfSpecies);
-            console.log("whereLives "+this.whereLives);
-        };
+        // this.move = function() {
+        //     console.log("animal moves");
+        // }
+        // this.describe = function() {
+        //     return `${this.typeOfSpecies}  ${this.whereLives}`;
+        // };
     }
         var dog = new Animal("개과", "육지");
         var cat = new Animal("고양이과","육지");
